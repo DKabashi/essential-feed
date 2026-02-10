e2ad50fbae0278e454d69580f83c5c9defeb1e54
Donat Kabashi <donatkabashi@gmail.com>
2026-02-10 16:28:22 +0100

    test init remotefeedloader

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -8,6 +8,18 @@
 import XCTest
 @testable import EssentialFeed
 
+class RemoteFeedLoader: FeedLoader {
+    let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func loadFeed(completion: @escaping (LoadFeedResult) -> Void) {
+        
+    }
+}
+
 final class EssentialFeedTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -18,19 +30,10 @@ final class EssentialFeedTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_init_urlIsNotNil() {
+        let sut = RemoteFeedLoader(url: URL(string: "https://google.com")!)
+        
+        XCTAssertNotNil(sut.url)
     }
 
 }
