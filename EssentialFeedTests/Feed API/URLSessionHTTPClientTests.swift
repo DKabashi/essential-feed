315956c621912aee34362da22101c282f5050a54
Donat Kabashi <donatkabashi@gmail.com>
2026-05-01 21:52:08 +0200

    test get from url resumes with datatask

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,31 +15,55 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: URLRequest(url: url)) { _, _, _ in }
+        session.dataTask(with: URLRequest(url: url)) { _, _, _ in }.resume()
     }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
 // subclass based testing
     
-    func test_get_usesCorrectURL() {
+    func test_getFromURL_resumesDataTaskWithURL() {
         let session = URLSessionSpy()
         let sut = URLSessionHTTPClient(session: session)
+        let dataTask = URLSessionDataTaskWithCounter()
         let url = URL(string: "http://any-url.com")!
+        session.stub(url: url, dataTask: dataTask)
         
         sut.get(from: url)
         
-        XCTAssertEqual([url], session.receivedURLs)
+        XCTAssertEqual(dataTask.counter, 1)
     }
     
     final class URLSessionSpy: URLSession {
-        var receivedURLs = [URL?]()
+        var receivedStubs = [URL: Stub]()
         
-        override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(request.url)
-            return FakeURLSessionDataTask()
+        struct Stub {
+            let dataTask: URLSessionDataTask
         }
         
-        class FakeURLSessionDataTask: URLSessionDataTask { }
+        func stub(url: URL, dataTask: URLSessionDataTask) {
+            receivedStubs[url] = Stub(dataTask: dataTask)
+        }
+        
+        override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
+            if let url = request.url, let stub = receivedStubs[url] {
+                return stub.dataTask
+            }
+            
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    // MARK: DataTasks
+    final class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() { }
+    }
+    
+    final class URLSessionDataTaskWithCounter: URLSessionDataTask {
+        var counter: Int = 0
+        
+        override func resume() {
+            counter += 1
+        }
     }
 }
