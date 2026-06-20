58a8c05c4788f6bd6c7751b8c36650089d241818
Donat Kabashi <donatkabashi@gmail.com>
2026-06-20 13:43:50 +0200

    Create `LoadFeedFromCacheUseCaseTests` and move `FeedStoreSpy` to the cache tests module as a helper

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -130,45 +130,6 @@ final class CacheFeedUseCaseTests: XCTestCase {
         return (sut: sut, store: feedStore)
     }
     
-    private class FeedStoreSpy: FeedStore {
-        private var deletionCompletion: ActionCompletion?
-        private var insertionCompletion: ActionCompletion?
-        
-        private(set) var receivedMessages = [FeedStoreAction]()
-        
-        enum FeedStoreAction: Equatable {
-            case deleteCachedFeed
-            case insert(items: [LocalFeedImage], timestamp: Date)
-        }
-            
-        func deleteCachedFeed(completion: @escaping ActionCompletion) {
-            receivedMessages.append(.deleteCachedFeed)
-            deletionCompletion = completion
-        }
-        
-        func completeCacheDeletion(with error: NSError) {
-            deletionCompletion?(error)
-        }
-        
-        func completeCacheDeletionWithSuccess() {
-            deletionCompletion?(nil)
-        }
-        
-        func completeInsertion(with error: NSError) {
-            insertionCompletion?(error)
-        }
-        
-        func completeCacheInsertionWithSuccess() {
-            insertionCompletion?(nil)
-        }
-        
-        func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping ActionCompletion) {
-            receivedMessages.append(.insert(items: items, timestamp: timestamp))
-            insertionCompletion = completion
-        }
-    }
-
-    
     private func uniqueFeedItem() -> FeedImage {
         return FeedImage(id: UUID(), url: anyURL())
     }
---
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+import EssentialFeed
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotReciveAnyMessageUponCreation() {
+        let (_, feedStore) = makeSut()
+        
+        XCTAssertEqual(feedStore.receivedMessages, [])
+    }
+    
+    
+    private func makeSut(timestamp: Date = .now, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let feedStore = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: feedStore, createTimestamp: {
+            return timestamp
+        })
+        checkForMemoryLeaks(for: feedStore, file: file, line: line)
+        checkForMemoryLeaks(for: sut, file: file, line: line)
+        return (sut: sut, store: feedStore)
+    }
+    
+}
