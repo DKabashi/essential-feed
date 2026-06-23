f4ba17a6426e7a8335cd0ebfc97d1f69aaa783f5
Donat Kabashi <donatkabashi@gmail.com>
2026-06-23 10:08:37 +0200

    Add `ValidateFeedCacheUseCaseTests` to separate the concerns of loading and edge case system modifying like deleting

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+import EssentialFeed
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotReciveAnyMessageUponCreation() {
+        let (_, feedStore) = makeSut()
+        
+        XCTAssertEqual(feedStore.receivedMessages, [])
+    }
+    
+    private func makeSut(timestamp: Date = .now, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let feedStore = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: feedStore, createTimestamp: {
+            return timestamp
+        })
+        checkForMemoryLeaks(for: feedStore, file: file, line: line)
+        checkForMemoryLeaks(for: sut, file: file, line: line)
+        return (sut: sut, store: feedStore)
+    }
+}
