521560aae6d5d6ddee71398f283b38733ea75a66
Donat Kabashi <donatkabashi@gmail.com>
2026-06-23 11:46:02 +0200

    Refactor `LocalFeedLoader` to group cases in extensions and remove the unnecessary `deleteExpiredCache`

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -15,6 +15,15 @@ public final class LocalFeedLoader {
         self.createTimestamp = createTimestamp
     }
     
+    private func isExpired(timestamp: Date) -> Bool {
+        guard let expiredDate = calendar.date(byAdding: .day, value: validExpireDays, to: timestamp) else {
+            return true
+        }
+        return Date() > expiredDate
+    }
+}
+
+extension LocalFeedLoader {
     public func save(feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         feedStore.deleteCachedFeed { [weak self] deletionError in
             guard let self else { return }
@@ -26,6 +35,16 @@ public final class LocalFeedLoader {
         }
     }
     
+    private func cache(feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
+        feedStore.insert(feed.localFeed, timestamp: createTimestamp(), completion: { [weak self] insertionError in
+            guard self != nil else { return }
+            
+            completion(insertionError)
+        })
+    }
+}
+    
+extension LocalFeedLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         feedStore.retrieve { [weak self] result in
             guard self != nil else { return }
@@ -37,44 +56,21 @@ public final class LocalFeedLoader {
             }
         }
     }
+}
     
+extension LocalFeedLoader {
     public func validateCache() {
         feedStore.retrieve { [weak self] result in
             guard let self else { return }
             switch result {
             case .failure:
-                deleteExpiredCache { _ in }
+                feedStore.deleteCachedFeed { _ in }
             case .success((_, let timestamp)) where isExpired(timestamp: timestamp):
-                deleteExpiredCache { _ in }
+                feedStore.deleteCachedFeed { _ in }
             case .success:
                 break
             }
         }
-    }
-    
-    private func deleteExpiredCache(completion: @escaping (LoadResult) -> Void) {
-        feedStore.deleteCachedFeed { deletionError in
-            if let deletionError {
-                completion(.failure(deletionError))
-            } else {
-                completion(.success([]))
-            }
-        }
-    }
-    
-    private func isExpired(timestamp: Date) -> Bool {
-        guard let expiredDate = calendar.date(byAdding: .day, value: validExpireDays, to: timestamp) else {
-            return true
-        }
-        return Date() > expiredDate
-    }
-    
-    private func cache(feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
-        feedStore.insert(feed.localFeed, timestamp: createTimestamp(), completion: { [weak self] insertionError in
-            guard self != nil else { return }
-            
-            completion(insertionError)
-        })
     }
 }
 
