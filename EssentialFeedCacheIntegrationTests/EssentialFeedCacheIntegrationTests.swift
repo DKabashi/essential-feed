3c329c6c7c1515894313fc779caaf87b3dadc74e
Donat Kabashi <donatkabashi@gmail.com>
2026-06-26 10:14:31 +0200

    Load in cache integration delivered inserted items on non empty cache

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -2,7 +2,17 @@ import XCTest
 import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
-
+    
+    override func setUp() {
+        super.setUp()
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         
@@ -20,6 +30,32 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 3.0)
     }
     
+    func test_load_deliversInsertedItemsOnNonEmptyCache() {
+        let sutToInsert = makeSUT()
+        let sutToDelete = makeSUT()
+        let items = [uniqueFeedItem()]
+        
+        let exp1 = expectation(description: "Wait for insertion to finish")
+        sutToInsert.save(feed: items) { insertionError in
+            XCTAssertNil(insertionError)
+            exp1.fulfill()
+        }
+        wait(for: [exp1], timeout: 3.0)
+        
+        let exp2 = expectation(description: "Wait for load to finish")
+        sutToDelete.loadFeed { result in
+            switch result {
+            case .success(let receivedItems):
+                XCTAssertEqual(receivedItems, items)
+            default:
+                XCTFail("Expected items: \(items), but got \(result) instead")
+            }
+            exp2.fulfill()
+        }
+        
+        wait(for: [exp2], timeout: 3.0)
+    }
+    
     private func makeSUT() -> LocalFeedLoader {
         let bundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = testSpecificStoreURL()
@@ -28,6 +64,18 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         checkForMemoryLeaks(for: sut)
         checkForMemoryLeaks(for: store)
         return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifact()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifact()
+    }
+    
+    private func deleteStoreArtifact() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
