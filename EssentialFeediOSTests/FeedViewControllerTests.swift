b6c6404290672fac8f1b1f10349b07b199a12ff0
Donat Kabashi <donatkabashi@gmail.com>
2026-07-06 15:21:35 +0200

    Decouple tests from `refreshControl` third party implementation detail in case we want to replace it with custom refresh in the future

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -43,15 +43,15 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
-    func test_pullToRefresh_loadsFeed() {
+    func test_userInitiatedFeedLoad_loadsFeed() {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
         
-        sut.refreshControl?.simulatePullToRefresh()
+        sut.simulateUserInitiatedFeedLoad()
         XCTAssertEqual(loader.loadCallCount, 2)
         
-        sut.refreshControl?.simulatePullToRefresh()
+        sut.simulateUserInitiatedFeedLoad()
         XCTAssertEqual(loader.loadCallCount, 3)
     }
     
@@ -73,19 +73,19 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
     
-    func test_pullToRefresh_showsLoadingIndicator() {
+    func test_userInitiatedFeedLoad_showsLoadingIndicator() {
 //        TODO: Fix the breaking changes
 //        let (sut, _) = makeSUT()
 //
-//        sut.refreshControl?.simulatePullToRefresh()
+//        sut.simulateUserInitiatedFeedLoad()
 //
 //        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
     
-    func test_pullToRefresh_hidesLoadingIndicatorOnLoadingComplete() {
+    func test_userInitiatedFeedLoad_hidesLoadingIndicatorOnLoadingComplete() {
         let (sut, loader) = makeSUT()
 
-        sut.refreshControl?.simulatePullToRefresh()
+        sut.simulateUserInitiatedFeedLoad()
         loader.completeFeedLoading()
 
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
@@ -117,6 +117,12 @@ final class FeedViewControllerTests: XCTestCase {
         func completeFeedLoading() {
             completions[0](.success([]))
         }
+    }
+}
+
+private extension FeedViewController {
+    func simulateUserInitiatedFeedLoad() {
+        refreshControl?.simulatePullToRefresh()
     }
 }
 
