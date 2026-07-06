2daeb8c1c950196676b3c38c80982cf9d92dae6e
Donat Kabashi <donatkabashi@gmail.com>
2026-07-06 11:14:28 +0200

    Initialize `FeedViewController` does not load feed

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,5 +1,22 @@
 import XCTest
 
+class FeedViewController {
+    
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        
+    }
+}
+
 final class FeedViewControllerTests: XCTestCase {
 
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    class LoaderSpy {
+        private(set) var loadCallCount = 0
+    }
 }
