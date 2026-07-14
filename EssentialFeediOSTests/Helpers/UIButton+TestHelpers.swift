d90a911a505ae60dc58da4bc954153bf12fd0a85
Donat Kabashi <donatkabashi@gmail.com>
2026-07-14 11:00:18 +0200

    Move code to their own files and group them in MVC and helper folders

---
+++ EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIButton {
+    func simulateRetryButtonTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
