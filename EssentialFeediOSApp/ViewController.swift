5f19acd11f0a14678b2e36cb71ca3ea21c60ad12
Donat Kabashi <donatkabashi@gmail.com>
2026-07-21 10:52:30 +0200

    Add 'EssentialFeediOSApp' target to run the app UI

---
+++ EssentialFeediOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeediOSApp
+//
+//  Created by Donat Kabashi on 7/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
