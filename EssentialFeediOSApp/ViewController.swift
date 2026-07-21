3a510377bc60001758c8452d6245112c1097ff14
Donat Kabashi <donatkabashi@gmail.com>
2026-07-21 11:06:25 +0200

    Remove  storyboard references and start with `ViewController` as the main one

--- EssentialFeediOSApp/ViewController.swift
+++ EssentialFeediOSApp/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  EssentialFeediOSApp
-//
-//  Created by Donat Kabashi on 7/21/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
 
