c3e3407df6ea5e74a968e819a836c748fa790313
Donat Kabashi <donatkabashi@gmail.com>
2026-07-28 11:27:21 +0200

    Add `EssentialFeedApp` target

---
+++ EssentialFeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeedApp
+//
+//  Created by Donat Kabashi on 7/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
