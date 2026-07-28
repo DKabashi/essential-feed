01b930962330db1eb243f7d8c4958fc33d492a59
Donat Kabashi <donatkabashi@gmail.com>
2026-07-28 11:39:05 +0200

    Add the `FeedViewController` as initial screen in the app

--- EssentialFeedApp/SceneDelegate.swift
+++ EssentialFeedApp/SceneDelegate.swift
@@ -1,4 +1,6 @@
 import UIKit
+import EssentialFeediOS
+import EssentialFeed
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -6,13 +8,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
-        let vc = ViewController()
-        
         let window = UIWindow(windowScene: scene)
+        
+        let client = URLSessionHTTPClient()
+        let remoteFeedLoader = RemoteFeedLoader(url: serverURL(), client: client)
+
+        let vc = FeedUIComposer.composeWith(feedLoader: remoteFeedLoader, imageLoader: ImageLoaderPlaceholder())
         window.rootViewController = vc
         window.makeKeyAndVisible()
         
         self.window = window
+    }
+    
+    final class ImageLoaderPlaceholder: FeedImageDataLoader {
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
+            return DummyTask()
+        }
+    }
+    
+    struct DummyTask: FeedImageDataLoaderTask {
+        func cancel() {
+        }
+    }
+    
+    private func serverURL() -> URL {
+        return URL(
+            string:
+                "https://essentialdeveloper.com/feed-case-study/test-api/feed"
+        )!
     }
 }
 
