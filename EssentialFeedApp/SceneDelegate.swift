3c86d4e195962b6703694d1453164a861b2c2bf2
Donat Kabashi <donatkabashi@gmail.com>
2026-07-28 14:56:59 +0200

    Add a placeholder implementation of `FeedImageDataLoader` to make the UI work for now

--- EssentialFeedApp/SceneDelegate.swift
+++ EssentialFeedApp/SceneDelegate.swift
@@ -13,29 +13,56 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let client = URLSessionHTTPClient()
         let remoteFeedLoader = RemoteFeedLoader(url: serverURL(), client: client)
 
-        let vc = FeedUIComposer.composeWith(feedLoader: remoteFeedLoader, imageLoader: ImageLoaderPlaceholder())
+        let vc = FeedUIComposer.composeWith(feedLoader: remoteFeedLoader, imageLoader: URLSessionFeedImageDataLoader())
         window.rootViewController = vc
         window.makeKeyAndVisible()
         
         self.window = window
     }
-    
-    final class ImageLoaderPlaceholder: FeedImageDataLoader {
-        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
-            return DummyTask()
-        }
-    }
-    
-    struct DummyTask: FeedImageDataLoaderTask {
-        func cancel() {
-        }
-    }
-    
+
     private func serverURL() -> URL {
         return URL(
             string:
-                "https://essentialdeveloper.com/feed-case-study/test-api/feed"
+                "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed"
         )!
     }
 }
 
+private final class URLSessionFeedImageDataLoader: FeedImageDataLoader {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct InvalidDataError: Error {}
+    private struct Task: FeedImageDataLoaderTask {
+        let wrapped: URLSessionDataTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                }
+                
+                guard
+                    let data,
+                    let response = response as? HTTPURLResponse,
+                    (200..<300).contains(response.statusCode)
+                else {
+                    throw InvalidDataError()
+                }
+                
+                return data
+            })
+        }
+        task.resume()
+        return Task(wrapped: task)
+    }
+}
