a405159da94c1ec818f16b61c7696b300e7bdff6
Donat Kabashi <donatkabashi@gmail.com>
2026-07-28 16:34:52 +0200

    Implement the UI for the `ErrorView`

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -2,6 +2,33 @@ import UIKit
 
 public final class ErrorView: UIView {
     public var message: String?
+
+    private let errorLabel = UILabel()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupErrorLabel()
+    }
+    
+    private func setupView() {
+        backgroundColor = .red
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+    
+    private func setupErrorLabel() {
+        addSubview(errorLabel)
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.text = message
+        errorLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        errorLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        errorLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        errorLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 public final class FeedViewController: UITableViewController {
