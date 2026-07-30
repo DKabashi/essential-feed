d7d5126470f603dc23f038f752e683ec622f8d75
Donat Kabashi <donatkabashi@gmail.com>
2026-07-30 10:26:02 +0200

    Display loader on `didStartLoadingImage`

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -1,21 +1,51 @@
 import XCTest
 
-final class FeedImagePresenter {
-    
+struct FeedImageViewModel {
+    let isLoading: Bool
 }
 
-final class FeedImagePresenterTests: XCTest {
+protocol FeedImageView {
+    func display(_ model: FeedImageViewModel)
+}
+
+final class FeedImagePresenter {
+    private let view: FeedImageView
+    
+    init(view: FeedImageView) {
+        self.view = view
+    }
+    
+    func didStartLoadingImage() {
+        view.display(FeedImageViewModel(isLoading: true))
+    }
+}
+
+final class FeedImagePresenterTests: XCTestCase {
     
     func test_init_displaysNoMessage() {
         let view = ViewSpy()
-        _ = FeedImagePresenter()
+        _ = FeedImagePresenter(view: view)
         
         XCTAssertEqual(view.messages, [])
     }
     
-    
-    final class ViewSpy {
-        private(set) var messages = [String]()
+    func test_didStartLoadingImage_displaysLoader() {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
         
+        sut.didStartLoadingImage()
+        XCTAssertEqual(view.messages, [.display(isLoading: true)])
+    }
+    
+    final class ViewSpy: FeedImageView {
+        enum Message: Equatable {
+            case display(isLoading: Bool)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func display(_ model: FeedImageViewModel) {
+            messages.append(.display(isLoading: model.isLoading))
+        }
     }
 }
