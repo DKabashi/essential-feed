850925c42051a5ad04032aa415abffd3d9cad7e8
Donat Kabashi <donatkabashi@gmail.com>
2026-07-30 11:19:47 +0200

    `FeedImagePresneter` `didStartLoadingImage` hides retry button

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -1,11 +1,21 @@
 import XCTest
 
+
+// TODO: Refactor to use the laodingview
 struct FeedLoadingImageViewModel {
     let isLoading: Bool
 }
 
 protocol FeedLoadingImageView {
     func display(_ model: FeedLoadingImageViewModel)
+}
+
+struct RetryViewModel {
+    let shouldRetry: Bool
+}
+
+protocol RetryView {
+    func display(_ model: RetryViewModel)
 }
 
 struct FeedImageViewModel<Image> {
@@ -27,14 +37,17 @@ protocol FeedImageView {
 final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
     private let imageView: View
     private let loadingView: FeedLoadingImageView
+    private let retryView: RetryView
     
-    init(imageView: View, loadingView: FeedLoadingImageView) {
+    init(imageView: View, loadingView: FeedLoadingImageView, retryView: RetryView) {
         self.imageView = imageView
         self.loadingView = loadingView
+        self.retryView = retryView
     }
     
     func didStartLoadingImage(_ model: FeedImageViewModel<Image>) {
         imageView.display(FeedImageViewModel(image: nil, location: model.location, description: model.description))
+        retryView.display(RetryViewModel(shouldRetry: false))
         loadingView.display(FeedLoadingImageViewModel(isLoading: true))
     }
 }
@@ -47,7 +60,7 @@ final class FeedImagePresenterTests: XCTestCase {
         XCTAssertEqual(view.messages, [])
     }
     
-    func test_didStartLoadingImage_displaysInitialFeedImageDataAndLoader() {
+    func test_didStartLoadingImage_displaysInitialFeedImageDataAndNoRetryButtonAndLoader() {
         let (sut, view) = makeSUT()
         
         let model = FeedImageViewModel<ImageSpy>(image: nil, location: "Loc", description: "Desc")
@@ -55,21 +68,23 @@ final class FeedImagePresenterTests: XCTestCase {
         
         XCTAssertEqual(view.messages, [
             .display(image: model.image, location: model.location, description: model.description),
+            .display(shouldRetry: false),
             .display(isLoading: true)
         ])
     }
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter<ViewSpy, ImageSpy>, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedImagePresenter(imageView: view, loadingView: view)
+        let sut = FeedImagePresenter(imageView: view, loadingView: view, retryView: view)
         checkForMemoryLeaks(for: view, file: file, line: line)
         checkForMemoryLeaks(for: sut, file: file, line: line)
         return (sut, view)
     }
     
-    final class ViewSpy: FeedLoadingImageView, FeedImageView {
+    final class ViewSpy: FeedLoadingImageView, FeedImageView, RetryView {
         enum Message: Equatable {
             case display(isLoading: Bool)
+            case display(shouldRetry: Bool)
             case display(
                 image: ImageSpy?,
                 location: String?,
@@ -85,6 +100,10 @@ final class FeedImagePresenterTests: XCTestCase {
         
         func display(_ model: FeedImageViewModel<ImageSpy>) {
             messages.append(.display(image: model.image, location: model.location, description: model.description))
+        }
+        
+        func display(_ model: RetryViewModel) {
+            messages.append(.display(shouldRetry: model.shouldRetry))
         }
     }
     
